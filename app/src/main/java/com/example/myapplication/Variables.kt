45f40a974040c3114fabc920fc8032cefe74853d
Suchil19 <jlsuchil@gmail.com>
2024-06-16 13:54:56 -0600

    Primer commit

---
+++ app/src/main/java/com/example/myapplication/Variables.kt
@@ -0,0 +1,27 @@
+package com.example.myapplication
+
+/**
+ * Esto es un comentario
+ * Variables númericas
+ */
+
+fun main(){
+    //Valores Enteros - Int
+    val age:Int = 32
+    //Long -9,223,372,036,854,775,807 a 9,2223,372,036,854,8775,807 - Long
+    val example:Long = 30
+    //Valores Flotantes - Float
+    val exampleFloat:Float= 30.5f
+    //Double Ocupan mas memoria en el dispositivo
+    val doubleExample:Double = 3241.3123123
+}
+/**
+ * Variables Alfanimericas - Guardan
+ */
+
+//Char - solo soporta 1 caracter
+val charExample1:Char = 'e'
+val charExample2:Char = '2'
+val charExample3:Char = '@'
+// String acepta cualquier tipo
+val stringExample:String = "Guardia Nacional"
